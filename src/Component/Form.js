656ad9ab171f7b57eec8fbc56911d51f39739ac1
Janaki04 <telugujanaki412@gmail.com>
2021-12-28 17:09:56 +0530

    first

---
+++ src/Component/Form.js
@@ -0,0 +1,58 @@
+import React, {useState}  from 'react'
+import './style.css'
+
+ function Form() {
+  const [inputs, setInputs] = useState({
+    name:"",
+    department:"",
+    rating:""
+  });
+
+  const [output, setOutputs] = useState([])
+
+  const handleChange = (event) => {
+     setInputs({...inputs,[event.target.name]:event.target.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setOutputs([...output,{...inputs,id:output.length}])
+    setSubmit(true)
+}
+const[Submit, setSubmit]=useState(false)
+const back=()=>{
+setSubmit(false)
+}
+return (
+<div>
+    <div>{Submit?null:
+    <form onSubmit={handleSubmit}>
+    <h1 className='heading'>EMPLOYEE FEEDBACK FORM</h1>
+      <label className='text1'>Name:
+      <input className='box1' type="text" name="name" 
+      value={inputs.name} onChange={handleChange}/>
+      </label>
+      <label className='text2'> Department:
+        <input className='box1' type="text" name="department"
+         value={inputs.department} onChange={handleChange}/>
+        </label>
+        <label className='text3'>Rating:
+      <input className='box1' type="number" name="rating" 
+      value={inputs.rating} onChange={handleChange}/>
+      </label>
+        <input  className='box2' type="submit" />
+    </form>}
+    </div>
+    <div>{Submit?
+    <div className='box4'>
+    { output.map( item =>(
+    <h5 className='text4' key={item.id}> Name : {item.name} ; Department : {item.department} ; Rating : {item.rating} </h5>
+    ))}
+      {Submit?<input className='back' onClick={back} type="button" value="Go back"/>: null }
+      </div>:null}
+    </div>
+    </div>
+  )
+}
+export default Form
+
